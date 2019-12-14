eee257dd4cc2417665d18f9758059c8009d30159
Aaron <Aaron@As-MacBook-Pro.local>
2019-12-13 23:12:35 -0800

    added parser class, retrieved details from elements in parse_response

---
+++ parser.py
@@ -0,0 +1,14 @@
+class Parser:
+    def __init__(self):
+        pass
+
+    def parse_title(self, title):
+        pass
+    def parse_link(self, title):
+        pass
+    def parse_price(self, title):
+        pass
+    def parse_time(self, title):
+        pass
+    def parse_number_of_bedroom(self, title):
+        pass
--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as soup
+from parser.py import Parser
 
 # apt/housing 10km away from V6T1Z4 with 2 bedrooms unfurnished
 BASE_URL = 'https://vancouver.craigslist.org/search/'
@@ -9,6 +10,7 @@ class Scraper:
     def __init__ (self):
         self.page = ''
         self.postings = {}
+        parse = Parser()
 
     def get(self, link): 
         try:
@@ -24,7 +26,26 @@ class Scraper:
         return url[:-1]
     
     def parse_postings(self, page):
-        list_postings = page_soup.find_all("p", class_="result-info")
+        list_postings = page.find_all("p", class_="result-info")
+
+        result = []
+        #Assume that every post has a title, price tag, date time and link
+        for x in list_postings:
+            single_post = {}
+            title = x.find("a", class_="result-title hdrlnk").text
+            link = x.find("a", class_="result-title hdrlnk")['href']
+            price = x.find("span", class_="result-price").text
+            time = x.find("time")['datetime']
+            numberOfBedroom = x.find("span", class_="housing") 
+            numberOfBedroom = numberOfBedroom if numberOfBedroom == None else numberOfBedroom.text
+            single_post['title'] = title
+            single_post['link'] = link
+            single_post['price'] = price
+            single_post['time'] = time
+            single_post['numberOfBedroom'] = numberOfBedroom
+            result.append(single_post)
+
+        return result
 
     def set_soup(self, page):
         self.page = page
